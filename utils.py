51358f1bfe5c13f5687cf6d07cc676a0c12024ed
student <jakub.mamut@gmail.com>
2025-06-05 18:01:45 +0200

    chore: fix styling errors

--- utils.py
+++ utils.py
@@ -1,11 +1,16 @@
-def add (a: int , b: int) -> int :
+"""Super cool code :3"""
+def add(a: int, b: int) -> int:
+    """This code is for adding"""
     return a + b
 
-def subtract (a: int , b: int) -> int :
+def subtract(a: int, b: int) -> int:
+    """This code is for substracting"""
     return a - b
 
-def multiply (a: int , b: int) -> int :
+def multiply(a: int, b: int) -> int:
+    """This code is for multiplying"""
     return a * b
 
-def divide ( a: int , b : int ) -> float :
-     return a / b
+def divide(a: int, b: int) -> float:
+    """This code is for dividing"""
+    return a / b
